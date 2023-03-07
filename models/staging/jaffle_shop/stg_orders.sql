8104ebdeba47a978dca0ebb403181315fa9298c2
fpetralba <fpetralba@worldremit.com>
2023-03-07 11:20:37 +0800

    Commit several changes on the tutorial exercises

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -3,5 +3,4 @@ select
     user_id as customer_id,
     order_date,
     status
-
-from _prv_fraud.test_dbt_orders
+from {{ source('jaffle_shop', 'test_dbt_orders') }}
--- models/stg_customers.sql
+++
@@ -1,6 +0,0 @@
-select
-    id as customer_id,
-    first_name,
-    last_name
-
-from _prv_fraud.test_dbt_customers
